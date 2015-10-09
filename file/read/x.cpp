58a6737d4e0f608e663086b728414e6ce8d41369
Samitha Hewawasam <eupdate@users.noreply.github.com>
2015-10-09 16:54:13 +0530

    Creating objects of files

--- file/read/x.cpp
+++ file/read/x.cpp
@@ -8,23 +8,25 @@
 using namespace std;
 using namespace boost::filesystem;
 
+int i = 0;
+
 class FileFactory {
 public:
-    void setFile(string name, string path);
+    void setFile(string name, string path, int i);
     FileFactory();
 private:
     string name;
     string path;
-    
-    
-
 };
 
 FileFactory::FileFactory(void) {
 
+	boost::filesystem::path dir("objects");
+	boost::filesystem::create_directory(dir); 
+
 }
 
-void FileFactory::setFile(string name, string path) {
+void FileFactory::setFile(string name, string path, int i) {
     name = name;
     path = path;
     
@@ -33,8 +35,13 @@ void FileFactory::setFile(string name, string path) {
     ifstream getFile(name);
     
    while(getline(getFile, line)) {
-         
-	cout << line << endl;
+   
+   
+   string dir_path = "objects/" + to_string(i);
+
+	boost::filesystem::path dir(dir_path);
+	boost::filesystem::create_directory(dir);        
+
 
    }
 
@@ -45,9 +52,18 @@ void FileFactory::setFile(string name, string path) {
 int main() {
     FileFactory file;
     for ( boost::filesystem::recursive_directory_iterator end, dir("."); dir != end; ++dir ) {
-        file.setFile(
-            dir->path().filename().native(),
-            dir->path().native()
-        );
+                   
+        if(is_regular_file(*dir)){
+        
+        i++;
+		file.setFile(
+		    dir->path().filename().native(),
+		    dir->path().native(),
+		    i
+		);     
+        
+        }
+  
+
     }
 }
