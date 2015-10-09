5bcd7e95dd93b2e3859ab89cf1fc98d94369d9e4
Samitha Hewawasam <eupdate@users.noreply.github.com>
2015-10-09 15:14:21 +0530

    Added native function to the steFile method && erros fixed

--- file/read/x.cpp
+++ file/read/x.cpp
@@ -9,16 +9,12 @@ using namespace std;
 using namespace boost::filesystem;
 
 class FileFactory {
-
 public:
     void setFile(string name, string path);
     FileFactory();
-
 private:
     string name;
     string path;
-
-
 };
 
 FileFactory::FileFactory(void) {
@@ -26,21 +22,19 @@ FileFactory::FileFactory(void) {
 }
 
 void FileFactory::setFile(string name, string path) {
-
     name = name;
     path = path;
-
+    
+    cout << name <<endl;
 }
 
 
 int main() {
-
     FileFactory file;
-
     for ( boost::filesystem::recursive_directory_iterator end, dir("."); dir != end; ++dir ) {
-
-
-        file.setFile(dir->path().filename(), "fsf");
-
+        file.setFile(
+            dir->path().filename().native(),
+            dir->path().native()
+        );
     }
 }
