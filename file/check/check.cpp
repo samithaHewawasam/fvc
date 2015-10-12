d32cd4ff3feac7fa9e243d60abae516b765f4a89
Samitha Hewawasam <eupdate@users.noreply.github.com>
2015-10-12 16:40:20 +0530

    Added check file and stuffs

---
+++ file/check/check.cpp
@@ -0,0 +1,87 @@
+#include <string>
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+
+string returnSingleFromFileLine(string file, int num) {
+
+    ifstream getFile(file.c_str());
+    string line1;
+    int index = 0;
+    while (getline(getFile, line1)) {
+        index++;
+        if(num == index)
+            break;
+
+    }
+
+    return line1;
+
+}
+
+int readlastLine(string file) {
+
+    int index3 = 0;
+    ifstream indexLog(file.c_str());
+    string line;
+    while (getline(indexLog, line)) {
+        index3++;
+        if (indexLog.eof())
+            break;
+
+    }
+    return index3;
+}
+
+void fileRead() {
+
+    int getTheLineNumber = 0;
+    if(readlastLine("a.txt") > readlastLine("b.txt"))
+        getTheLineNumber = readlastLine("a.txt");
+    else
+        getTheLineNumber = readlastLine("b.txt");
+
+    ofstream patch;
+    patch.open("a.patch", ios_base::app);
+
+    // b.txt = previous file in the corresponding object
+    // a.txt = actual file in working directory
+
+    for(int i = 1; i <= getTheLineNumber; i++) {
+
+        if(returnSingleFromFileLine("a.txt", i) == returnSingleFromFileLine("b.txt", i)) {
+
+            patch << "+" << returnSingleFromFileLine("b.txt", i) << endl;
+
+        } else if(returnSingleFromFileLine("a.txt", i) != returnSingleFromFileLine("b.txt", i)) {
+
+            if(!returnSingleFromFileLine("a.txt", i).empty() && returnSingleFromFileLine("b.txt", i).empty()) {
+
+                patch << "+" << returnSingleFromFileLine("a.txt", i) << endl;
+
+            } else if(!returnSingleFromFileLine("b.txt", i).empty() && returnSingleFromFileLine("a.txt", i).empty()) {
+
+                patch << "-" << returnSingleFromFileLine("b.txt", i) << endl;
+
+            }else{
+            
+                patch << "+" << returnSingleFromFileLine("a.txt", i) << endl;
+            
+            }
+
+        }
+
+
+    }
+}
+
+int main() {
+
+    fileRead();
+
+}
+
+
+
